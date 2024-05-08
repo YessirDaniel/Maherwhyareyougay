b05201679475262561fa9a1ea621bfd2e425bf59
YessirDaniel <137095684+YessirDaniel@users.noreply.github.com>
2024-05-08 02:39:01 +0000

    Pending changes exported from your codespace

---
+++ Test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+printf("hi");
+//hi max what are u doing 
+'''wait max how do i run the code
+i dont think you can without local access
+or you could copy paste onto codespace
+whats local access
+like a local ide
+and we dont have local ide
+:(
+start a gofundme
+alo
+wait how do i even run the code i cant
+exactly
+wait how do i run regularly? 
+you would need to get vscode
+no like i right click on my file it says run python file in terminal
+well usually you can just run in terminal but you could also do that too
+wait how do u run in terminal i forgot
+for python its just python followed by the file name and any command line args
+for c i think you have to make and then ./filename
+but when i do make test. it says no rule to make test.c STOp
+HELLO U THERE?
+i think u have to include stdio.h
+I DONT THINK THAT MADE A DIFFERENCE
+actually I think make is premade for cs50
+you can search up how to compile it i think, im pretty sure I remember them referencing clang
+wait i can run
+or you can get an extention
+
+gcc -o my_executable my_program.c 
+'''
---
+++ testing.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("hi\n");
+    return 0;
+}
+// gimme access to terminal im pretty sure I sent a request, idk where it goes tho
+//AH SHIT IT FUCKING WORKS. WAIT HOW DO I GIVE U ACCESS TO TERMINAL
+// theres a liveshare tab at the bottom, idk if that does anything but maybe I MADE IT READ WRITE 
+// DID THAT DO ANYTHING
+//works. USE THE gcc -o thing it actually works
+// it worksah shit. SEE IM CRAZY. SOMEHOW I FIGURED IT OUT. TOTALLY NOT ASKING CHATGPT
+//CHATGPT I LOVE U
